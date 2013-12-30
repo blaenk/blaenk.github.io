41acccc045f412be5cefd78e0defd76dfba1c845
Jorge Israel Peña <jorgepblank@gmail.com>
2013-12-30 15:18:32 -0800

    shed fork dependency

--- src/Site/Filters.hs
+++ src/Site/Filters.hs
@@ -4,10 +4,9 @@ module Site.Filters (
 
 import Hakyll
 
-import Site.ShellFilter
-
 -- TODO: preferably use patterns/rules here
 sassCompiler :: Compiler (Item String)
 sassCompiler = loadBody (fromFilePath "scss/screen.scss")
                  >>= makeItem
-                 >>= withItemBody (shellFilter "sass -s --scss -I provider/scss/ --cache-location generated/scss")
+                 >>= withItemBody (unixFilter "sass" args)
+  where args = ["-s", "--scss", "-I", "provider/scss/", "--cache-location", "generated/scss"]
