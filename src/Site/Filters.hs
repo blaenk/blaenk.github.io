ae3eb1b0172f8639c5f48468fb751154d008201b
Jorge Israel Peña <jorge.israel.p@gmail.com>
2014-05-04 17:24:41 -0700

    unixFilter now handles windows; don't use upper bounds unless necessary

--- src/Site/Filters.hs
+++ src/Site/Filters.hs
@@ -3,13 +3,11 @@ module Site.Filters (
 ) where
 
 import Hakyll
-import Site.Util (procArgs)
 
 -- TODO: preferably use patterns/rules here
 sassCompiler :: Compiler (Item String)
 sassCompiler = loadBody (fromFilePath "scss/screen.scss")
                  >>= makeItem
-                 >>= withItemBody (unixFilter cmd args)
-  where (cmd, args) = procArgs "sass"
-                        ["-s", "--scss", "-I", "provider/scss/",
-                         "--cache-location", "generated/scss"]
+                 >>= withItemBody (unixFilter "sass" args)
+  where args = ["-s", "--scss", "-I", "provider/scss/",
+                "--cache-location", "generated/scss"]
