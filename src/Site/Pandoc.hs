53d0d36ff7379ab6945c16ffd4cce363c1f9e980
Jorge Israel Peña <jorge.israel.p@gmail.com>
2014-05-12 15:21:34 -0700

    hyphenation support

--- src/Site/Pandoc.hs
+++ src/Site/Pandoc.hs
@@ -34,6 +34,8 @@ import Control.Monad ((>=>))
 
 import Control.Applicative ((<$>))
 
+import Text.Hyphenation (hyphenate, english_US)
+
 pandocFeedCompiler :: Compiler (Item String)
 pandocFeedCompiler =
   pandocCompilerWithTransform readerOptions writerOptions' (walk ignoreTOC . walk removeTOCMarker)
@@ -46,11 +48,27 @@ pandocCompiler streams = do
 
   let transformer =
         return . abbreviations abbrs
+        >=> return . hyphens
         >=> return . codeBreak
         >=> pygments streams
         >=> return . tableOfContents alignment
 
   pandocCompilerWithTransformM readerOptions writerOptions transformer
+
+hyphens :: Pandoc -> Pandoc
+hyphens = walk hyphenation
+
+hyphenation :: Inline -> Inline
+hyphenation (Str str) =
+  Str . shy . hyphenate english_US $ str
+  where shy [] = []
+        shy [x] = x
+        shy (x:xs) = merge x (shy xs)
+
+        merge "-" a = '-' : a
+        merge [] a = '\x00ad' : a
+        merge (x:xs) a = x : merge xs a
+hyphenation x = x
 
 codeBreak :: Pandoc -> Pandoc
 codeBreak = walk breakChars
