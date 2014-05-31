3b37898e73a34ec840c5c416f11196fbb07114f0
Jorge Israel Peña <jorge.israel.p@gmail.com>
2014-05-30 17:18:42 -0700

    use scss instead, more direct

--- src/Site/Compilers.hs
+++ src/Site/Compilers.hs
@@ -45,7 +45,7 @@ contentCompiler content channels streams =
 sassCompiler :: Compiler (Item String)
 sassCompiler = loadBody (fromFilePath "scss/screen.scss")
                  >>= makeItem
-                 >>= withItemBody (unixFilter "sass" args)
-  where args = ["-s", "--scss", "-I", "provider/scss/",
+                 >>= withItemBody (unixFilter "scss" args)
+  where args = ["-s", "-I", "provider/scss/",
                 "--cache-location", "generated/scss"]
 
